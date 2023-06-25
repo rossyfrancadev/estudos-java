25881c317f1982be44b89ad646d0ff684f9321f3
Rossywan França <rossyfrancadev@gmail.com>
2023-06-25 19:05:07 -0300

    first commit

---
+++ estudos-api/src/main/java/com/franca/estudosapi/EstudosApiApplication.java
@@ -0,0 +1,13 @@
+package com.franca.estudosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosApiApplication.class, args);
+	}
+
+}
---
+++ estudos-api/src/test/java/com/franca/estudosapi/EstudosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franca.estudosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
